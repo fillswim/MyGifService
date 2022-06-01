5540da3d3066c6d7fc573be8c40da3d6419f5b02
Filatov Oleg <fillswim@gmail.com>
2022-06-01 21:47:51 +0300

    Initial commit

---
+++ src/main/java/com/example/mygifservice/TtGifServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygifservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtGifServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtGifServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mygifservice/TtGifServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygifservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtGifServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
