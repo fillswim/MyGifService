696a2c1d6f3020fe93c033069eb5cb88fb9c4912
Filatov Oleg <fillswim@gmail.com>
2022-06-04 22:41:05 +0300

    Update client

--- src/main/java/com/example/mygifservice/clients/GiphyClient.java
+++ src/main/java/com/example/mygifservice/clients/GiphyClient.java
@@ -4,11 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @FeignClient(name = "GiphyClient", url = "${giphy.url.general}")
 public interface GiphyClient {
 
     @GetMapping("/random")
-    String getGif(
+    Optional<String> getGif(
             @RequestParam("api_key") String appId,
             @RequestParam("tag") String tag);
 
--- src/main/java/com/example/mygifservice/clients/RateClient.java
+++ src/main/java/com/example/mygifservice/clients/RateClient.java
@@ -1,23 +1,24 @@
 package com.example.mygifservice.clients;
 
-import com.example.mygifservice.models.Rates;
+import com.example.mygifservice.models.RatesResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "FOERClient", url = "${oxr.url.general}")
-public interface FOERClient {
+import java.util.Optional;
+
+@FeignClient(name = "RateClient", url = "${oxr.url.general}")
+public interface RateClient {
 
     @GetMapping("/latest.json")
-    Rates getLatestRates(@RequestParam("app_id") String appId);
-
-
-    @GetMapping("/historical/{date}.json")
-    Rates getHistoricalRates(
-            @PathVariable String date,
+    Optional<RatesResponse> getResponseRates(
             @RequestParam("app_id") String appId);
 
+    @GetMapping("/historical/{date}.json")
+    Optional<RatesResponse> getResponseRates(
+            @PathVariable String date,
+            @RequestParam("app_id") String appId);
 
 
 }
