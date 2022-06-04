3064827b96a098471cf6f8e562a8b42b6f09bba9
Filatov Oleg <fillswim@gmail.com>
2022-06-04 22:41:44 +0300

    Update controller

--- src/main/java/com/example/mygifservice/controllers/GifController.java
+++ src/main/java/com/example/mygifservice/controllers/GifController.java
@@ -1,30 +1,37 @@
 package com.example.mygifservice.controllers;
 
-import com.example.mygifservice.services.GiphyService;
+import com.example.mygifservice.services.GifService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+
 @RestController
 @RequestMapping("/api/gifs")
 @Api(tags = "Gif Controller")
+@Validated
 public class GifController {
-    private final GiphyService giphyService;
+    private final GifService gifService;
 
-    public GifController(GiphyService giphyService) {
-        this.giphyService = giphyService;
+    public GifController(GifService gifService) {
+        this.gifService = gifService;
     }
 
     @ApiOperation(value = "Get gif by currency code")
     @GetMapping(value = "/{currencyCode}", produces = MediaType.IMAGE_GIF_VALUE)
-    public ResponseEntity<byte[]> getGif(@PathVariable("currencyCode") String currencyCode) {
+    public ResponseEntity<byte[]> getGif(
+            @Valid @Pattern(regexp = "[A-Z]{3}", message = "The format of the currency code must be ХХХ")
+            @PathVariable("currencyCode") String currencyCode) {
 
-        byte[] array = giphyService.getGif(currencyCode);
+        byte[] array = gifService.getGif(currencyCode);
 
         ResponseEntity<byte[]> responseEntity = ResponseEntity
                 .ok()
